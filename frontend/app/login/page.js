e0e180452564b7a31f27cbc360f59a7fc17d35b8
amin04 <aminmithun1992@gmail.com>
2025-01-05 01:51:22 +0600

    login page issue solved

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react"
 import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
@@ -35,7 +37,7 @@ export default function Login() {
 						<label htmlFor="email" className="block text-sm font-medium text-gray-700">
 							Email
 						</label>
-						<input type="email" id="email" placeholder="Enter your email" className={`w-full px-4 py-2 mt-1 border ${errors.email ? "border-red-500" : "border-gray-300"} rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500`} {...register("email")} />
+						<input type="text" id="email" placeholder="Enter your email" className={`w-full px-4 py-2 mt-1 border ${errors.email ? "border-red-500" : "border-gray-300"} rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500`} {...register("email")} />
 						{errors.email && <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>}
 					</div>
 
